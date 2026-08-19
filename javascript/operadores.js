9ad9efc38e3e92d70ee7e96b61e7d07a1b3ba254
camillycsantos <camilly.c.santos@ba.estudante.senai.br>
2026-08-19 14:58:10 -0300

    Node e Funcoes

---
+++ javascript/operadores.js
@@ -0,0 +1,20 @@
+// OPERADORES LÓGICOS
+//== != > < >= <= ===
+
+let idade = 20;
+console.log(idade >= 18);
+
+//OPERADORES ARITMETICOS
+let valorCompra = 35.90;
+let valorPago = 50;
+let troco = valorPago - valorCompra;
+
+console.log("Troco a ser devolvido: R$" + troco + ",00" );
+
+//OPERAÇÃO COMPOSTA
+let numero1 = 4;
+let numero2 = 4;
+
+console.log(
+    numero1 % 2 === 0;
+)
---
+++ javascript/variaveis.js
@@ -0,0 +1,20 @@
+//VARIÁVEIS DO TIPO "STRING"
+let nome = "Camis";
+console.log("Olá, " + nome + "! Seja Bem-Vindo ao curso Front-End!");
+
+nome = "Maria Joaquina";
+console.log("Olá, " + nome + "! Seja Bem-Vindo ao curso de Front-End!");
+
+//VARIÁVEIS DO TIPO "NUMBER"
+
+let anoAtual = 2026;
+let anoNascimento = 2007;
+let idade = anoAtual - anoNascimento;
+console.log("Você tem " + idade + " anos.");
+
+let matematica = 7
+let portugues = 8
+let ciencias =  9
+
+let media = (matematica + portugues + ciencias) / 3;
+console.log("Sua média final é " + media + ". Parabéns!");
